2cc84159a8de1b4baae79717b6fccea8da1168b8
Anuja Morvankar <anuja.m@benchmarksolution.com>
2020-06-22 21:13:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# animal_planet_app
+ 
---
+++ android/app/src/main/kotlin/com/animalplanet/animal_planet_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.animalplanet.animal_planet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
